a39dc544cd3bfc9517dde236b5a4fbaf9c5cb138
Paasdag <deanroks0@gmail.com>
2025-08-20 22:42:18 +0200

    Solution #2?

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("readme.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
 setup(
     name='tfd-py',
     version='0.1',    
@@ -8,7 +11,7 @@ setup(
     author='Dean',
     license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International',
     packages=find_packages(include=['tfd_py', 'tfd_py.*']),
-    long_description=open("readme.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.10",
     install_requires=[
